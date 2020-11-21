f9cd72321f7a7b32f169087c7441ae94630ade36
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2020-11-20 19:09:22 -0800

    Log-in Page

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000000"
+    },
+    secondary: {
+      main: "#3C4858"
+    }
+  },
+  typography: {
+    h1: {
+      fontFamily: 'Nunito',
+      fontSize: '64px'
+    },
+    h2: {
+      fontFamily: 'Nunito',
+      fontSize: '36px'
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
