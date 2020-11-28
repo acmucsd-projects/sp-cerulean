8a5df3d8e55d56667053f510beddcfd822d8805c
Justin Yao Du <justinyaodu@gmail.com>
2020-11-27 18:29:22 -0800

    Add script to populate database with fake data

---
+++ fakedata.js
@@ -0,0 +1,117 @@
+const fs = require("fs");
+const dbAccess = require("./db");
+
+const filename = "./fake-data.json";
+
+// Database table names.
+const USER_TABLE = "usertable";
+const EVENT_TABLE = "eventtable";
+const ATTENDANCE_TABLE = "attendance";
+
+/**
+ * Create an INSERT query from an object which has key-value pairs corresponding
+ * to column names and column values.
+ * 
+ * @param tableName The name of the table to insert into.
+ * @param rowObj    An object representing the row to insert.
+ * @return          An array of arguments which can be spread into query().
+ */
+makeInsertQuery = (tableName, rowObj) => {
+    const columnNames = [];
+    const queryArguments = [];
+
+    for (const [key, value] of Object.entries(rowObj)) {
+        columnNames.push(key);
+        queryArguments.push(value);
+    }
+
+    // Generate a string "$1, $2, ..." with the right number of parameters.
+    const queryParameters = Array.from(
+        queryArguments,
+        (x, i) => "$" + (i + 1)
+    ).join(", ");
+
+    const queryString = `INSERT INTO ${tableName}`
+        + ` (${columnNames.join(", ")})`
+        + ` VALUES (${queryParameters})`;
+
+    return [queryString, queryArguments];
+}
+
+/**
+ * Return whether a table is empty.
+ * 
+ * @param client    The database client.
+ * @param tableName The table to query.
+ * @return          Whether the table is empty.
+ */
+tableIsEmpty = async (client, tableName) => {
+    const result = await client.query(`SELECT * FROM ${tableName} LIMIT 1`);
+    return result.rows.length === 0;
+}
+
+fs.readFile(filename, "utf-8", async function(err, data) {
+    if (err) {
+        throw err;
+    }
+
+    const fakeData = JSON.parse(data);
+
+    await dbAccess.connectToPG();
+
+    /*
+    // DANGER: delete existing data before inserting fake data.
+    await dbAccess.db.query("DELETE FROM " + USER_TABLE);
+    await dbAccess.db.query("DELETE FROM " + EVENT_TABLE);
+    await dbAccess.db.query("DELETE FROM " + ATTENDANCE_TABLE);
+    */
+
+    // Ensure that all tables are empty before proceeding.
+    for (const table of [USER_TABLE, EVENT_TABLE, ATTENDANCE_TABLE]) {
+        if (!(await tableIsEmpty(dbAccess.db, table))) {
+            throw new Error(`Table '${table}' already contains data.`
+                + " Fake data will not be inserted.");
+        }
+    }
+
+    for (const user of fakeData.users) {
+        const row = {
+            uuid: user.uuid,
+            email: user.email,
+            firstname: user.firstName,
+            lastname: user.lastName,
+            graduationyear: user.graduationYear.toString(),
+            major: user.major,
+            points: user.points.toString(),
+        };
+
+        await dbAccess.db.query(...makeInsertQuery(USER_TABLE, row));
+    }
+
+    for (const ev of fakeData.events) {
+        const row = {
+            uuid: ev.uuid,
+            eventstart: ev.start,
+            eventend: ev.end,
+            organization: ev.organization,
+            title: ev.title,
+            description: ev.description,
+            eventlocation: ev.location,
+        }
+
+        await dbAccess.db.query(...makeInsertQuery(EVENT_TABLE, row));
+    }
+
+    for (const attendance of fakeData.attendances) {
+        const row = {
+            user_id: attendance.userUuid,
+            event_id: attendance.eventUuid,
+            attendance_time: attendance.timestamp,
+            as_staff: attendance.asStaff.toString(),
+        };
+
+        await dbAccess.db.query(...makeInsertQuery(ATTENDANCE_TABLE, row));
+    }
+
+    console.log("Fake data inserted.");
+});
