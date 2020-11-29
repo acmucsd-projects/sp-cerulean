64bf9c772c58b8ed41670f2727f7b0676572a297
Justin Yao Du <justinyaodu@gmail.com>
2020-11-29 09:34:01 -0800

    Use WHERE clause in event attendance query

--- api/event.js
+++ api/event.js
@@ -3,20 +3,18 @@ const express = require("express");
 const router = express.Router();
 /**
  * GET Route that handles single event attendance
- * @param eventName takes in an event name
+ * @param eventId the ID of the event
  * @returns json object with number of attendances and number of staff members
  */
-router.get("/eventAttend/:eventName", async (req, res) => {
+router.get("/eventAttend/:eventId", async (req, res) => {
   let attendanceCount = 0;
   let staffMembers = 0;
   try {
-    dbAccess.db.query("SELECT * FROM attendance", (error, response) => {
+    dbAccess.db.query("SELECT * FROM attendance WHERE id = $1", [req.params.eventId], (error, response) => {
       response.rows.forEach((value) => {
-        if (value.eventname == req.params.eventName) {
-          attendanceCount++;
-          if (value.asstaff == "true") {
-            staffMembers++;
-          }
+        attendanceCount++;
+        if (value.asstaff === "true") {
+          staffMembers++;
         }
       });
       res.status(200).json({
