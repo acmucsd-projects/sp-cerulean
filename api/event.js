a4ff607e33f809b25d63339b3b3fdacad86bba0e
Justin Yao Du <justinyaodu@gmail.com>
2020-11-30 15:39:02 -0800

    Implement API route for average points

--- api/event.js
+++ api/event.js
@@ -2,6 +2,7 @@ const { db } = require("../db");
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+
 /**
  * GET Route that handles single event attendance
  * @param eventId the ID of the event
@@ -22,6 +23,51 @@ router.get("/eventAttend/:eventId", async (req, res) => {
         numberOfAttendances: attendanceCount,
         staffMembers: staffMembers,
       });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("There was an error");
+  }
+});
+
+/**
+ * GET route which returns an event's attendees' mean and median number of points.
+ *
+ * @param eventId The ID of the event.
+ * @returns A JSON object with the mean and median point values.
+ */
+router.get("/averagePoints/:eventId", async (req, res) => {
+  const queryString = "WITH attendee (uuid) AS (SELECT (user_id) FROM attendance WHERE event_id = $1)"
+    + " SELECT (points) FROM usertable WHERE uuid IN (SELECT * FROM attendee)";
+
+  try {
+    db.query(queryString, [req.params.eventId], (error, response) => {
+      if (error) {
+        throw error;
+      }
+
+      const points = response.rows.map(row => parseInt(row.points));
+
+      if (points.length === 0) {
+        res.status(200).json({
+          meanPoints: null,
+          medianPoints: null,
+        });
+      } else {
+        points.sort();
+        const mean = points.reduce((a, b) => a + b) / points.length;
+
+        const middleIndex = Math.floor(points.length / 2);
+        const median = (points.length % 2 == 0)
+          ? (points[middleIndex] + points[middleIndex + 1]) / 2
+          : points[middleIndex];
+
+        res.status(200).json({
+          meanPoints: mean,
+          medianPoints: median,
+        })
+      }
+
     });
   } catch (err) {
     console.error(err);
