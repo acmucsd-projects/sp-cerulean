de9b8bff1da537d077dd1c59d82b105324b0a76b
Advay Sharma <sharmaadvay0@gmail.com>
2020-12-12 23:27:11 -0800

    created bar chart

--- client/src/components/AverageChart.js
+++ client/src/components/AverageChart.js
@@ -1,31 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
+import axios from "axios";
 
 const AverageChart = () => {
-  const data = {
-    labels: ["Day 1", "Day 2", "Day 3"],
-    datasets: [
-      {
-        label: "Average Attendance",
-        data: [5, 10, 15],
-        borderWidth: 2,
-      },
-    ],
-  };
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
+  const [state, setState] = useState({
+    data: null,
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
           },
-        },
-      ],
+        ],
+      },
     },
+  });
+
+  const getData = async () => {
+    const config = {
+      headers: {
+        "x-auth-token": "",
+      },
+    };
+    await axios
+      .get("/api/event/eventInfo/0/5", config)
+      .catch((err) => console.error(err))
+      .then(async (eventData) => {
+        var titles = [];
+        let pointDataArray = [];
+
+        const info = eventData.data[0];
+        titles.push(info.title);
+
+        //modify this route to send an array of objects
+        // const body = JSON.stringify({});
+        // await axios
+        //   .get("/api/event/averagePoints", body, config)
+        //   .then((pointData) => {
+        //     const tableData = {
+        //       labels: titles,
+        //       datasets: [
+        //         {
+        //           label: "Average Points Per Event",
+        //           data: pointDataArray,
+        //           borderWidth: 2,
+        //         },
+        //       ],
+        //     };
+        //     setState({ ...state, data: tableData });
+        //   })
+        //   .catch((err) => console.error(err));
+      });
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="chart">
-      <Bar data={data} options={options} />
+      {state.data != null ? (
+        <div>
+          <h1>{console.log(state)}</h1>
+          <Bar data={state.data} options={state.options} />
+        </div>
+      ) : (
+        <h1>{console.log(state)}</h1>
+      )}
     </div>
   );
 };
